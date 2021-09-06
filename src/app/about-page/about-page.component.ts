9ed1cd91ebf42e0e2c0a57ba9563f0d26e0f1e02
Ryan Tee <ryanfx1998@gmail.com>
2021-09-06 23:19:15 +0200

    Added Experience page

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.css']
+  styleUrls: ['./about-page.component.css'],
 })
 export class AboutPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
